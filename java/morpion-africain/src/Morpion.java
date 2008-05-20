a28baf0587342818e6ff1d99acf931fa43a35ec6
guillaume.dd <guillaume.dd@151d7b76-814d-0410-bea2-d78fd1274ee2>
2008-05-20 14:30:52 +0000

    Modifications des classes Jeu.java et Morpion.java

--- java/morpion-africain/src/Morpion.java
+++ java/morpion-africain/src/Morpion.java
@@ -1,4 +1,30 @@
 
-public class Morpion {
-
+public class Morpion extends Jeu{
+	private CaseMorpion cases;
+	
+	public void creerCases(){
+		
+	}
+	public void creerJoueurs(){
+		
+	}
+	public void initialiser(){
+		int i;
+		for(i=0;i<6;i++){
+			joueurCourant.initialiser();
+			changerJoueurCourant();
+		}
+	}
+	public void jouer(){
+		while(!fini()){
+			joueurCourant.jouer();
+			changerJoueurCourant();
+		}
+	}
+	public boolean fini(){
+		return true;
+	}
+	public void changerJoueurCourant(){
+		joueurCourant=joueurCourant.getJoueurSuivant();
+	}
 }
