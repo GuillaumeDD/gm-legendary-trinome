f10bf89c61e5e5585a90b6c05f4d6eb0bd330c51
zacharie.ales <zacharie.ales@151d7b76-814d-0410-bea2-d78fd1274ee2>
2008-05-20 13:41:41 +0000

    Classe Joueur

---
+++ java/morpion-africain/src/Joueur.java
@@ -0,0 +1,17 @@
+
+public class Joueur extends Personne{
+
+	private Joueur joueurPrecedent = new Joueur();
+	private Joueur joueurSuivant   = new Joueur();
+	
+	public Joueur(){
+	}
+	
+	public Joueur(StringBuffer nomJoueur){
+		setNom(nomJoueur);
+	}
+	
+	public void setJoueurSuivant(Joueur j){
+		joueur
+	}
+}
