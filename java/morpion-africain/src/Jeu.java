5f0c5200756b97f17b62ffb1c52d2c37ffca22b0
guillaume.dd <guillaume.dd@151d7b76-814d-0410-bea2-d78fd1274ee2>
2008-05-20 13:51:27 +0000

    Ajout de la classe Jeu.java

---
+++ java/morpion-africain/src/Jeu.java
@@ -0,0 +1,13 @@
+
+public abstract class Jeu {
+	Joueur joueurs;
+	int nombreDeJoueurs;
+	Joueur joueurCourant;
+	
+	public abstract void creerCases();
+	public abstract void creerJoueurs();
+	public abstract void initialiser();
+	public abstract void jouer();
+	public abstract boolean fini();
+	public abstract void changerJoueurCourant(Joueur j);
+}
