ef338bc7fa245cdb4e0de3d92e9b1d6d4bbc0511
guillaume.dd <guillaume.dd@151d7b76-814d-0410-bea2-d78fd1274ee2>
2008-05-22 10:15:46 +0000

    Ajout du support clavier

--- trunk/java/morpion-africain/src/Morpion.java
+++ trunk/java/morpion-africain/src/Morpion.java
@@ -1,4 +1,4 @@
-
+import java.io.*;
 public class Morpion extends Jeu{
 	private CaseMorpion cases[] = new CaseMorpion[9];
 //on redefinit un joueurCourant de type JoueurMorpion car il doit pouvoir utiliser les méthodes initialiser() et jouer()
@@ -15,20 +15,36 @@ public class Morpion extends Jeu{
 		
 	}
 	public void initialiser(){
+		/*
 		int i;
 		for(i=0;i<6;i++){
 			joueurCourant.initialiser();
 			changerJoueurCourant();
 		}
+		*/
 	}
 	public void jouer(){
+		BufferedReader entree = new BufferedReader(new InputStreamReader(System.in));
+	
+		CaseMorpion origine,destination;
+		int o,d;
+		origine=new CaseMorpion();
+		destination=new CaseMorpion();
+		
+
 		while(!fini()){
-			joueurCourant.jouer();
+			try {
+		        o=Integer.parseInt(entree.readLine());
+		        System.out.println("Vous avez tape :"+o);
+			} catch( IOException e ) {
+			        e.printStackTrace();
+			}
+			((JoueurMorpion)joueurCourant).jouer(origine,destination);
 			changerJoueurCourant();
 		}
 	}
 	public boolean fini(){
-		return true;
+		return false;
 	}
 	public void changerJoueurCourant(){
 		joueurCourant=joueurCourant.getJoueurSuivant();
