244e4f697b1da4a1ffc880e23a35a544ac651472
guillaume.dd <guillaume.dd@151d7b76-814d-0410-bea2-d78fd1274ee2>
2008-06-09 12:53:39 +0000

    Renommage des fichiers main

--- java/morpion-africain/src/morpion/MainMorpionGraphique.java
+++ java/morpion-africain/src/morpion/MainMorpionGraphique.java
@@ -1,6 +1,6 @@
 package morpion;
 
-public class MainGraphique {
+public class MainMorpionGraphique {
 	  public static void main(String args[]){
 		  MorpionGraphique m = new MorpionGraphique();
 	  }
